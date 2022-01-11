d162d286061edb6bd229259c74a41d1062cf642e
Darren Wan <47572301+darren-wan@users.noreply.github.com>
2022-01-11 17:32:33 +0800

    Added logging to actions console

--- dist/index.js
+++ dist/index.js
@@ -11513,17 +11513,23 @@ try
   const isStrictOutput = JSON.parse(core.getInput('strict-output').toLowerCase());
 
   let semverMap = new Map();
+
+  console.log('Processing %d elements from list', versionList.length);
   
   versionList.forEach(version => {
     let sv = semver.parse(version, !isStrictParsing);
     if (sv != null) semverMap.set(sv, version);
   });
 
+  console.log('Found %d valid SemVer values', semverMap.size);
+
   let semverList = Array.from(semverMap.keys).sort(semver.rcompare);                              
 
   if (semverList.length > 0)
   {
     let latest = semverList[0];
+    console.log('Selected value %s (Original: %s)', latest, semverMap[latest]);
+    
     core.setOutput("latest", isStrictOutput ? latest.toString() : semverMap[latest]);
   }
   else if (Boolean(core.getInput('fail-on-empty')))
